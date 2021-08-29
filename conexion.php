f94b5f60bfb6a1c47c19e9dd4c846d287dc14fcd
Johan98-dev <johanalbarracin67@gmail.com>
2021-08-29 12:38:28 -0500

    actualización credenciales de la BD

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php 
 	function conectar(){
-		$conn_string = "host=ec2-52-203-74-38.compute-1.amazonaws.com port=5432 dbname=d7oqgl83bu9cuh user=fxkvnqgytipsea password= f0a705e340b75f7b50a0e03a7e5a7707f763cf29ea552a91a5924c47a1ba6f10";
+		$conn_string = "host=ec2-52-87-107-83.compute-1.amazonaws.com port=5432 dbname=de7na2kqrbqcbj user= sddbmcdcdectye password= b7aab0f22460f5a734f5d000680974eb7fb48d03f30137beee9413ca37a1d284";
 		$con = pg_connect($conn_string) or die ("Error al conectar".pg_error());
 		return $con;
 	}
