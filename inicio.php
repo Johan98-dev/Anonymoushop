bd2f819fbef1f7887a218bfcdf332d37f71439ce
Johan98-dev <johanalbarracin67@gmail.com>
2021-09-06 00:46:40 -0500

    Se modificó el Inicio.php

--- inicio.php
+++ inicio.php
@@ -4,7 +4,7 @@
 	<title>AnonymouShop</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" type="image/x-icon" href="imagenes/icono.png">
+	<link rel="shortcut icon" type="image/x-icon" href="/Cliente/imh/art.jpg">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 	<link type=" text/css" rel="stylesheet" href="css/Style.css"/>
@@ -13,82 +13,75 @@
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container-fluid">
 			<h4>AnonymouShop</h4>
-
 			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#">Deportes</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Tecnología</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Ropa</a>
-					</li>
-				</ul>
+				
 				<span class="navbar-text">
-					<a class="nav-link" href="loginC.php">Login</a>
-					<a class="nav-link" href="registrar.php">Registro</a>
+					
+					<a class="nav-link" href="loginC.php">
+						<span id="span1"></span>
+						<span id="span2"></span>
+						<span id="span3"></span>
+						<span id="span4"></span>
+					Login
+					</a>
+					<a class="nav-link" href="registrar.php">
+					    <span id="span1"></span>
+						<span id="span2"></span>
+						<span id="span3"></span>
+						<span id="span4"></span>
+						Registro
+					</a>
+					
+					
 				</span>
 			</div>
 		</div>
 	</nav>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<article class="article">
-					<h1>Bienvenidos a AnonymouShop
-					</h1>    
-				</article>
-			</div>
-		</div>
-		<br>
-		<div class="row">
-			<div class="col-md-4">
-				<div class="panel panel-default">
-
-					<div style="background-color: #D1B96B;">
-						<center><h3><b><i class="fas fa-dollar-sign"></i> imagen</b></h3></center>
-					</div>
-					<div style="background-color: #C4C7C6 ;">
-						<br>
-						descripcion
-					</div>
-
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="panel panel-default">
-					<div style="background-color: #D1B96B;">
-						<center><h3><b><i class="fas fa-dollar-sign"></i> imagen</b></h3></center>
-					</div>
-					<div style="background-color: #C4C7C6 ;">
-						<br>
-						descripcion
-					</div>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="panel panel-default">
-					<div style="background-color: #D1B96B;">
-						<center><h3><b><i class="fas fa-dollar-sign"></i> imagen</b></h3></center>
-					</div>
-					<div style="background-color: #C4C7C6 ;">
-						<br>
-						descripcion
-					</div>
-				</div>
-			</div>
-		</div>
-	</div> 
-	<!-- SECCION PIE DE PÁGINA -->
-        <footer class="pie-de-pagina text-center text-md-right bg-dark text-white">
-            <div class="container">
-                <p class="m-0 py-2" align="center">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Copyright © 2021. Todos los derechos reservados.</p>
-            </div>
-        </footer>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>       
-
+	<div class="bien"> <p>Bienvenidos a Anonymouhop</p></div>
+	<div class="container mt-5">
+<div class="row" style="justify-content: center;">
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="10" />
+        <input name="titulo" type="hidden" id="titulo" value="articulo 1" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="Cliente/img/xbox.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Producto 1</h5>
+                        <p class="card-text">Descripción - Precio 10$</p>
+                        
+                </div>
+        </form>
+</div>
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="20" />
+        <input name="titulo" type="hidden" id="titulo" value="articulo 2" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="Cliente/img/camiseta.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Producto 2</h5>
+                        <p class="card-text">Descripción - Precio 20$</p>
+                        
+                </div>
+        </form>
+</div>
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="30" />
+        <input name="titulo" type="hidden" id="titulo" value="articulo 3" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="Cliente/img/balon1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">Producto 3</h5>
+                        <p class="card-text">Descripción - Precio 30$</p>
+                        
+                </div>
+        </form>
+</div>
+</div>
+</div>
+	
+	
 </body>
 </html>        
